4eaf6e08b5f1e23c08747a9077077822bb0df97a
KCC NMA <gargiulo@hawaii.edu>
2017-07-12 14:33:53 -1000

    test initial gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,25 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-  return gulp.src('src/scss/**/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('dist/css'))
+    return gulp.src('src/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('dist/css'))
+})
+
+// HTML
+gulp.task('html', () => {
+    return gulp.src('src/**/*.html')
+        .pipe(gulp.dest('dist/'))
+})
+
+// IMAGES
+gulp.task('images', () => {
+    return gulp.src('src/img/**/*')
+        .pipe(gulp.dest('dist/img/'))
 })
 
 gulp.task('watch', function(){
-  gulp.watch('src/scss/**/*.scss', ['sass']); 
+    gulp.watch('src/scss/**/*.scss', ['sass']); 
+    gulp.watch('src/**/*.html', ['html']); 
+    gulp.watch('src/img/**/*', ['images']); 
 })
